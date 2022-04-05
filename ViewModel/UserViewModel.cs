3f1cd66a788d19cb822cdbc8d265208b9e0c8cd6
Diegownia <Pkorczak93@gmail.com>
2022-04-05 15:15:44 +0100

    Redoing the whole ComboBox

---
+++ ViewModel/UserViewModel.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SandpitWPF.Model;
+
+namespace SandpitWPF.ViewModel
+{
+    public class UserViewModel : BaseViewModel
+    {
+        private int _id;
+        private string _name;
+        private string _surname;
+        private int _age;
+        private UserModel _user;
+
+        public int ID 
+        {
+            get { return _id; }
+            set
+            {
+                _id = value;
+                NotifyPropertyChanged(nameof(ID));
+            }
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                _name = value;
+                NotifyPropertyChanged(nameof(Name));
+            }
+        }
+
+        public string Surname
+        {
+            get { return _surname; }
+            set
+            {
+                _surname = value;
+                NotifyPropertyChanged(nameof(Surname));
+            }
+        }
+
+        public int Age
+        {
+            get { return _age; }
+            set
+            {
+                _age = value;
+                NotifyPropertyChanged(nameof(Age));
+            }
+        }
+
+        public UserModel User
+        {
+            get { return _user; }
+            set 
+            { 
+                _user = value;
+                NotifyPropertyChanged(nameof(User));
+            }
+        }
+
+
+    }
+}
