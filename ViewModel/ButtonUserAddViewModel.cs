e15c3364084d8b6b0b702aef41141ea73405ccb9
Diegownia <Pkorczak93@gmail.com>
2022-04-08 15:59:24 +0100

    ehhh... nothing happenes here

---
+++ ViewModel/ButtonUserAddViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace SandpitWPF.ViewModel
+{
+    public class ButtonUserAddViewModel
+    {
+        //public ICommand AddUser{ get; private set; }
+
+        //public ButtonUserAddViewModel()
+        //{ 
+
+        //}
+
+
+
+
+    }
+}
