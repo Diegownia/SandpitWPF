a8dec637fef783864588bf08f98c46196e6c4720
Diegownia <pkorczak93@gmail.com>
2022-05-03 22:22:16 +0100

    meh...

---
+++ ViewModel/Dialog2ViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SandpitWPF.ViewModel
+{
+    public class Dialog2ViewModel
+    {
+
+    }
+}
